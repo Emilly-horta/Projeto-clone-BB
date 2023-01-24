c282a47331543dce21a10e795ed32910710d8787
Emilly <millyphb@gmail.com>
2023-01-23 22:19:58 -0300

    add bloco de beneficios

---
+++ components/Beneficios.js
@@ -0,0 +1,78 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Beneficios.module.scss";
+
+export default function Beneficios() {
+  return (
+    <section className={styles.container} id="Beneficios">
+      <div className={styles.wrap}>
+        <h1 className={styles.title}>
+          Vem ser cliente e aproveite os benefícios do BB
+        </h1>
+      </div>
+
+      <div className={styles.beneficioslista}>
+        <div className={styles.beneficioitens}>
+          <div className={styles.images}>
+            <Image
+              src="/src/assets/images/icon1.png"
+              alt="abertura digital"
+              width={70}
+              height={80}
+            />
+          </div>
+          <div className={styles.box}>
+            <h1>Abertura Digital</h1>
+            <p>De qualquer lugar, abra sua conta com poucos cliques</p>
+          </div>
+        </div>
+
+        <div className={styles.beneficioitens}>
+          <div className={styles.images}>
+            <Image
+              src="/src/assets/images/icon2.png"
+              alt="abertura digital"
+              width={70}
+              height={80}
+            />
+          </div>
+          <div className={styles.box}>
+            <h1>Tudo pelo App</h1>
+            <p>Todos os nossos produtos e serviços na palma da mão</p>
+          </div>
+        </div>
+
+        <div className={styles.beneficioitens}>
+          <div className={styles.images}>
+            <Image
+              src="/src/assets/images/icon3.png"
+              alt="Seguranca"
+              width={70}
+              height={80}
+            />
+          </div>
+          <div className={styles.box}>
+            {" "}
+            <h1>Segurança</h1>
+            <p>Conte com a segurança que o Banco do Brasil pode oferecer</p>
+          </div>
+        </div>
+
+        <div className={styles.beneficioitens}>
+          <div className={styles.images}>
+            <Image
+              src="/src/assets/images/icon4.png"
+              alt="conveniencia"
+              width={70}
+              height={80}
+            />
+          </div>
+          <div className={styles.box}>
+            <h1>Conveniência</h1>
+            <p>Conte com o apoio da nossa rede de agências no seu dia a dia</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Hero from "../components/Hero";
 import Head from 'next/head';
 import Layout from "../components/Layout";
+import Beneficios from "../components/Beneficios";
 
 export default function Home() {
   return (
     <Layout>
       <Hero />
+      <Beneficios/>
     </Layout>
   );
 }
