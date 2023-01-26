ea7fc8f23b167b62b65377e92f0c81713356ffb9
Emilly <millyphb@gmail.com>
2023-01-26 17:22:34 -0300

    add novo bloco de opcoes

---
+++ components/Opcoes.js
@@ -0,0 +1,53 @@
+import styles from "../styles/Opcoes.module.scss";
+
+export default function Beneficios() {
+  return (
+    <section className={styles.container} id="beneficios">
+      <div className={styles.wrap}>
+        <h1>Ao seu lado em cada momento da vida</h1>
+        <p>
+          Sempre tem uma solução do BB pra cuidar do que é valioso pra você.
+        </p>
+      </div>
+
+      <div className={styles.item}>
+        <ul className={styles.lista}>
+          <li>PATRIMÔNIO E FINANÇAS</li>
+          <li>CASA E FAMÍLIA</li>
+          <li>SAÚDE E BEM-ESTAR</li>
+          <li>LAZER E ENTRETENIMENTO</li>
+          <li>LAZER E ENTRETENIMENTO</li>
+        </ul>
+      </div>
+
+      <div className={styles.box}>
+        <div className={styles.opcoes}>
+          <div className={styles.img1}>
+            <h1>Open Finance</h1>
+            <ul>
+              <li>Aumente suas chances de ter mais limite aqui no BB</li>
+            </ul>
+          </div>
+
+          <div className={styles.img2}>
+            <h1>Minhas Finanças</h1>
+            <ul>
+              <li>Aproveite as vantagens de ter um Ourocard</li>
+            </ul>
+            <ul className={styles.img4}>
+              <h1>Seguro de veículos</h1>
+              <li>Praticidade e conforto onde e quando quiser</li>
+            </ul>
+          </div>
+
+          <div className={styles.img3}>
+            <h1>Investimentos</h1>
+            <ul>
+              <li>Quer saber quanto seu dinheiro pode render no BB?</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Head from 'next/head';
 import Layout from "../components/Layout";
 import Beneficios from "../components/Beneficios";
 import Vantagens from "../components/Vantagens";
+import Opcoes from "../components/Opcoes";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <Beneficios/>
       <Vantagens/>
+      <Opcoes/>
     </Layout>
   );
 }
